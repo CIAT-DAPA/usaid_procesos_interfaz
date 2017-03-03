7e640636db82a637d9be8b327f37edb61f788d50
Edward Guevara <edarague@gmail.com>
2017-03-03 14:50:05 -0500

    parse configuraciones

--- runMain.R
+++ runMain.R
@@ -29,18 +29,20 @@ path_save <- paste0(dirPrediccionOutputs, "probForecast", sep = "", collapse = N
 path_output <- paste0(dirPrediccionOutputs, "resampling", sep = "", collapse = NULL)
 path_output_sum <- paste0(path_output, "/summary", sep = "", collapse = NULL)
 dir_dssat <- 'C:/DSSAT46/'  ## its necessary to have the parameters .CUL, .ECO, .SPE Updated for running (calibrated the crop (Maize))
+dirCultivosInputs <-paste0(dirInputs, "cultivos/", sep = "", collapse = NULL)
+dirCultivosOutputs <-paste0(dirOutputs, "cultivos/", sep = "", collapse = NULL)
 
 ## Variables modelo Maiz
 dirModeloMaiz <- paste0(dirCurrent, "modeloMaiz/", sep = "", collapse = NULL)
-dirModeloMaizInputs <- paste0(dirInputs, "cultivo/maiz/", sep = "", collapse = NULL)
-dirModeloMaizOutputs <-paste0(dirOutputs, "cultivo/maiz/", sep = "", collapse = NULL)
+dirModeloMaizInputs <- paste0(dirInputs, "cultivos/maiz/", sep = "", collapse = NULL)
+dirModeloMaizOutputs <-paste0(dirOutputs, "cultivos/maiz/", sep = "", collapse = NULL)
 dir_run <- paste0(dirModeloMaizOutputs, "run/", sep = "", collapse = NULL)
 dir_soil <- paste0(dirModeloMaiz, "soils/CC.SOL", sep = "", collapse = NULL)
 
 ## Variables paquete arroz
 dirModeloArroz <- paste0(dirCurrent, "modeloMaiz/", sep = "", collapse = NULL)
-dirModeloArrozInputs <- paste0(dirInputs, "cultivo/arroz/", sep = "", collapse = NULL)
-dirModeloArrozOutputs <-paste0(dirOutputs, "cultivo/arroz/", sep = "", collapse = NULL)
+dirModeloArrozInputs <- paste0(dirInputs, "cultivos/arroz/", sep = "", collapse = NULL)
+dirModeloArrozOutputs <-paste0(dirOutputs, "cultivos/arroz/", sep = "", collapse = NULL)
 
 pathConstruct <- function(dirConstruct)
   {
@@ -64,39 +66,45 @@ pathConstruct(dir_save)
 pathConstruct(path_save)
 pathConstruct(path_output)
 pathConstruct(path_output_sum)
-pathConstruct(dirModeloMaizInputs)
+pathConstruct(dirCultivosOutputs)
+# pathConstruct(dirModeloMaizInputs)
 pathConstruct(dirModeloMaizOutputs)
 pathConstruct(dir_run)
-pathConstruct(dirModeloArrozInputs)
+# pathConstruct(dirModeloArrozInputs)
 pathConstruct(dirModeloArrozOutputs)
 
-CMDdirInputs <- paste0(gsub("/","\\\\",dirInputs), "\\\"")
+CMDdirInputs <- paste0(gsub("/","\\\\",dirPrediccionInputs), "\\\"")
 try(system(paste0(forecastAppDll,"-s \"prec\" -p \"",CMDdirInputs," -start 1981 -end 2013"), intern = TRUE, ignore.stderr = TRUE))
 try(system(paste0(forecastAppDll,"-wf -p \"",CMDdirInputs," -name \"daily\""), intern = TRUE, ignore.stderr = TRUE))
-try(system(paste0(forecastAppDll,"-fs -p \"",CMDdirInputs), intern = TRUE, ignore.stderr = TRUE))
+CMDdirInputs <- paste0(gsub("/","\\\\",dirInputs), "\\\"")
 try(system(paste0(forecastAppDll,"-fs -p \"",CMDdirInputs), intern = TRUE, ignore.stderr = TRUE))
 cat("\n")
 
 runPrediccion <- source(paste(dirForecast,'01_prediccion.R', sep = "", collapse = NULL))
 
-# runRemuestreo <- source(paste(dirForecast,'02_remuestreo.R', sep = "", collapse = NULL))
+runRemuestreo <- source(paste(dirForecast,'02_remuestreo.R', sep = "", collapse = NULL))
 
 ## Modelo maiz
-data_m_all = list.files(dir_response,full.names = T)
-data_d_all = list.files(dir_stations,full.names = T)
+setups = list.dirs(dirModeloMaizInputs,full.names = T)
 
-station_month_names = gsub('.csv','',list.files(dir_response))
-station_names = gsub('.csv','',list.files(dir_stations))
+for(x in 2:length(setups)){
+  setSplit <- strsplit(setups[x],"/")
+  longName <- setSplit[[1]][length(set_split[[1]])]
+  longNameSplit <- strsplit(longName,"_")
 
-for(x in 1:length(data_m_all)){
-  for(y in 1:length(data_d_all)){
-    region <- station_month_names[x]
-    name_csv <- paste0(region, "_", station_names[y], ".csv", sep = "", collapse = NULL)
-    cat(paste("\n\n Ejecutando modelo Maiz para region: \"", region, "\", estacion: \"", station_names[x], "\"\n", sep = ""))
-    dir_climate <- paste0(path_output, "/", station_names[x], sep = "", collapse = NULL)
-    # runModeloMaiz <- source(paste(dirModeloMaiz,'call_functions.R', sep = "", collapse = NULL))
+  hashStation <- longNameSplit[[1]][1]
+  hashCrop <- longNameSplit[[1]][2]
+  hashSoil<- longNameSplit[[1]][3]
+  hashDayRange <- longNameSplit[[1]][4]
+
+  cat(paste("\n\n Ejecutando modelo Maiz para estacion: \"", hashStation, "\" cultivar: \"", hashCrop, "\" suelo: \"", hashSoil, "\" rango de dias: \"", hashDayRange, "\"\n", sep = ""))
+  name_csv <- paste0(longName, ".csv", sep = "", collapse = NULL)
+  cat(name_csv)
+  dir_climate <- paste0(path_output, "/", hashStation, sep = "", collapse = NULL)
+  cat(dir_climate)
+  # runModeloMaiz <- source(paste(dirModeloMaiz,'call_functions.R', sep = "", collapse = NULL))
   }
-}
+
 cat("\n\n... modelo Maiz finalizado\n")
 
 
