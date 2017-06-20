fe5675bc8fcbf18ecd44eeec46dbc2ca397b4f35
aesquivel94 <a.esquivel@cgiar.org>
2017-06-20 15:45:14 -0500

    Correction to the run_cpt function in the configuration of the model - and automatic run for trimester in the department.

--- prediccionClimatica/03_CPT.R
+++ prediccionClimatica/03_CPT.R
@@ -1,4 +1,10 @@
-run_cpt<-function (dep, trim, x_predictors, x_modes,y_predicting,  y_modes, cca_modes, dir_output, transformation){
+# Esta funcion crea el bash para correr en la consola de linux cada corrida de CPT
+# x_predictors = predictores de la TSM generados con el codigo de las areas predictoras
+# y_predicting = archivos de cada departamento
+# dir_output = directorio de salida donde se guardan los resultados 
+# modes = archivo de configuraciones 
+run_cpt<-function (x_predictors, y_predicting, modes, dir_output){
+# cmd corresponde al bash que se guarda automaticamente con los outputs  
   cmd <- "@echo off
 
   (
@@ -45,25 +51,28 @@ run_cpt<-function (dep, trim, x_predictors, x_modes,y_predicting,  y_modes, cca_
   echo 0
   ) | CPT_batch.exe"
   
+  dep <- strsplit(tail(strsplit(y_predicting, "/")[[1]], 1), ".txt")[[1]]
+  trim <- strsplit(tail(strsplit(x_predictors, "/")[[1]], 1), ".txt")[[1]]
+  
+  modos <- read.csv(file = modes, header =TRUE, row.names = 1)
+  
   cmd<-gsub("%X_PREDICTORS%",x_predictors,cmd)
-  cmd<-gsub("%X_Modes%",x_modes,cmd)
+  cmd<-gsub("%X_Modes%",modos["x_modes",] ,cmd)
   cmd<-gsub("%Y_PREDICTING%",y_predicting,cmd)
-  cmd<-gsub("%Y_Modes%",y_modes,cmd)
-  cmd<-gsub("%CCA_Modes%",cca_modes,cmd)
+  cmd<-gsub("%Y_Modes%",modos["y_modes",],cmd)
+  cmd<-gsub("%CCA_Modes%",modos["cca_modes", ],cmd)
   cmd<-gsub("%RESULT_GI%",paste0(dir_output,"/GoodnessIndex_",dep, "_", trim,".txt"),cmd)
   cmd<-gsub("%Pearson%",paste0(dir_output,"/Pearsons_correlation_",dep, "_", trim,".txt"),cmd)
   cmd<-gsub("%2AFC%",paste0(dir_output,"/k_2AFC_Score_",dep, "_", trim,".txt"),cmd)
   cmd<-gsub("%Canonical%",paste0(dir_output,"/CanonicalCorrelations_",dep, "_", trim,".txt"),cmd)
   cmd<-gsub("%RESULT_PROB%",paste0(dir_output,"/ForecastProbabilities_",dep, "_", trim,".txt"),cmd)
+
   
-  
-  if(transformation == "no"){
-    cmd<-gsub("%Transformation%"," ",cmd)} else if(transformation != "no"){
+  if(modos["trasformation",] == "no"){
+    cmd<-gsub("%Transformation%"," ",cmd)} else if(modos["trasformation",] != "no"){
       cmd<-gsub("%Transformation%","541",cmd)}
   
-  #cmd <- gsub("/","\", cmd)
   write(cmd,"C:/Users/AESQUIVEL/Desktop/outputs/text.bat")
-  #system(cmd, ignore.stdout = T, show.output.on.console = F)
   system("C:/Users/AESQUIVEL/Desktop/outputs/text.bat", ignore.stdout = T, show.output.on.console = F)
 }
 
@@ -71,9 +80,50 @@ run_cpt<-function (dep, trim, x_predictors, x_modes,y_predicting,  y_modes, cca_
 
 
 
-
-run_cpt(dep = "cordoba", trim = "no", x_predictors = "C:/Users/AESQUIVEL/Desktop/s.txt",
-          x_modes = 5, y_predicting = "C:/Users/AESQUIVEL/Google Drive/Exp_2/dep/precip_cordoba.txt",
-          y_modes = 5, cca_modes = 3, dir_output = "C:/Users/AESQUIVEL/Desktop/outputs", transformation = "no")
+#### Leer el archivo de configuracines para extraer los modos de x, y, cca y si se transforma o no
 
 
+
+# directorio donde se guardan los archivos de la seleccion del area predictora
+root <- "C:/Users/AESQUIVEL/Desktop/selection_predictor_area" 
+Locations <- list.dirs(root, full.names = FALSE,  recursive = FALSE)
+
+
+# La idea es hacer aqui un lapply por departamento 
+
+function(x){
+  
+  # Completo
+  y_predicting <- paste0( root, "/", x ,"/precip/", list.files(path = paste0(root, "/", x ,"/precip/"), pattern = ".txt"))
+  # x hace referencia al departamento 
+  x_predictors <- paste0( root, "/", x ,"/tsm/", list.files(path = paste0(root, "/", x, "/", "tsm"), pattern = ".txt"))
+  
+  Conf<- paste0("C:/Users/AESQUIVEL/Desktop/conf/", x, "/", list.files(paste0("C:/Users/AESQUIVEL/Desktop/conf/", x, "/")))
+  
+  
+  
+  run_cpt(x_predictors = "C:/Users/AESQUIVEL/Desktop/selection_predictor_area/casanare/tsm/s1.txt", 
+          y_predicting = "C:/Users/AESQUIVEL/Desktop/selection_predictor_area/cordoba/precip/precip_cordoba.txt",
+          modes = "C:/Users/AESQUIVEL/Desktop/conf/cordoba/cordoba_s1.csv",
+          dir_output = "C:/Users/AESQUIVEL/Desktop/outputs")
+
+  
+  
+  mapply(run_cpt, x_predictors, y_predicting, Conf, dir_output, SIMPLIFY = FALSE)
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+run_cpt(x_predictors = "C:/Users/AESQUIVEL/Desktop/selection_predictor_area/casanare/tsm/s1.txt", 
+        y_predicting = "C:/Users/AESQUIVEL/Desktop/selection_predictor_area/cordoba/precip/precip_cordoba.txt",
+        modes = "C:/Users/AESQUIVEL/Desktop/conf/cordoba/cordoba_s1.csv",
+        dir_output = "C:/Users/AESQUIVEL/Desktop/outputs")
