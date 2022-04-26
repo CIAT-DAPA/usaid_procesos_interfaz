4f86093a1a42c9766e54b1408f9df1ceb4b46416
Daniel Esteban Guzmán <estebantoon@hotmail.com>
2022-04-25 19:58:13 -0500

    Upload raster files with auth tested

--- prediccionClimatica/raster_files_upload.R
+++ prediccionClimatica/raster_files_upload.R
@@ -2,23 +2,19 @@
 #http://localhost:8600/geoserver/rest/imports
 #url_geoserver_api <- "https://geo.aclimate.org/" #"http://localhost:8600/geoserver/rest/imports"
 url_geoserver_api <- "http://localhost:8600/geoserver/rest/imports"
+url_geoserver_api2 <- "http://172.17.0.3:8080/geoserver/rest/imports"
 #Upload and import the raster files to Geosever
 #The first POST() upload the raster files and prepare them to be import
 #The second POST() import the raster files
 upload_raster_files_to_geoserver <- function(geoserver_url_api){
 
   require(httr)
-  workspaceName <- paste0("aclimate_", countries_ids[currentCountry])
-  initialization <-
+  #workspaceName <- paste0("aclimate_", countries_ids[currentCountry]) #aclimate_61e59d829d5d2486e18d2ea9"
+  #initialization <-
   storeName <- "Seasonal"
-
-  body <- paste0('{
+  
+  body <- '{
     "import": {
-        "targetStore": {
-        "dataStore": {
-          "name":"'storeName, '"
-        }
-      },
         "targetWorkspace": {
           "workspace": {
               "name":"', workspaceName,'"
@@ -26,15 +22,34 @@ upload_raster_files_to_geoserver <- function(geoserver_url_api){
         },
         "data": {
           "type": "directory",
-          "location": "/data/aclimate_et/"
+          "location": "/opt/geoserver/data_dir/data"
         }
     }
-  }')
-  response <- POST(geoserver_url_api, content_type_json(), body = body)
+  }'
+
+  # body <- paste0('{
+  #   "import": {
+  #       "targetStore": {
+  #       "dataStore": {
+  #         "name":"'storeName, '"
+  #       }
+  #     },
+  #       "targetWorkspace": {
+  #         "workspace": {
+  #             "name":"', workspaceName,'"
+  #         }
+  #       },
+  #       "data": {
+  #         "type": "directory",
+  #         "location": "/data/aclimate_et/"
+  #       }
+  #   }
+  # }')
+  response <- POST(geoserver_url_api, content_type_json(), body = body, authenticate("admin", "geoserver"))
   import_id <- content(response)$import$id
-  POST(paste0(geoserver_url_api, "/", import_id))
+  POST(paste0(geoserver_url_api, "/", import_id), authenticate("admin", "geoserver"))
 }
 
-upload_raster_files_to_geoserver(url_geoserver_api)
+upload_raster_files_to_geoserver(url_geoserver_api2)
 
 cat("\n Raster files imported in Geoserver \n")
